5d3ecc0e1ba13348d3d56ac57e89c746d152215e
rescuevessel <dave@rescuevessel.com>
2021-02-09 13:02:18 -0500

    added hero 07

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
 import React from 'react'
 import './App.css'
 import lottie from 'lottie-web'
-import animationData from './lottie/MP_Hero_05_Build_v7c.json'
-import animationData2 from './lottie/MP_Hero_01_Build_v10.json'
-import animationData3 from './lottie/MP_Hero_01_Build_v14.json'
 
-let animObj1 = null
-
-let animObj2 = null
-
-let animObj3 = null
+import hero01 from './lottie/MP_Hero_01_Build_v14.json'
+import hero05 from './lottie/MP_Hero_05_Build_v7c.json'
+import hero07 from './lottie/MP_Hero07_Build_v1.json'
 
 class App extends React.Component {
 	componentDidMount() {
 		console.log('componentDidMount')
 
 		//call the loadAnimation to start the animation
-		animObj1 = lottie.loadAnimation({
-			container: this.animBox1, // the dom element that will contain the animation
+		lottie.loadAnimation({
+			container: this.hero01obj, // the dom element that will contain the animation
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
-			animationData: animationData, // the path to the animation json
+			animationData: hero01, // the path to the animation json
 		})
 
-		animObj2 = lottie.loadAnimation({
-			container: this.animBox2, // the dom element that will contain the animation
+		lottie.loadAnimation({
+			container: this.hero05obj, // the dom element that will contain the animation
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
-			animationData: animationData2, // the path to the animation json
+			animationData: hero05, // the path to the animation json
 		})
-		animObj3 = lottie.loadAnimation({
-			container: this.animBox3, // the dom element that will contain the animation
+		lottie.loadAnimation({
+			container: this.hero07obj, // the dom element that will contain the animation
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
-			animationData: animationData3, // the path to the animation json
+			animationData: hero07, // the path to the animation json
 		})
 	}
-	handleStop = () => {
-		animObj1.stop()
-	}
-	handlePlay() {
-		animObj2.play()
-	}
-	handleContinue() {
-		animObj3.play()
-	}
+
 	render() {
 		return (
 			<div>
@@ -55,19 +42,21 @@ class App extends React.Component {
 					<h2>Mixpanel / Hero 01 / Limitless Segmentation</h2>
 					<div
 						style={{ width: 600, margin: '0 auto' }}
-						ref={ref => (this.animBox3 = ref)}></div>
+						ref={ref => (this.hero01obj = ref)}></div>
 				</div>
-				<div className='anim'>
-					<h2>Mixpanel / Hero 01 / Limitless Segmentation</h2>
-					<div
-						style={{ width: 600, margin: '0 auto' }}
-						ref={ref => (this.animBox2 = ref)}></div>
-				</div>
+
 				<div className='anim'>
 					<h2>Mixpanel / Hero 05 / Scalable Infrastructure</h2>
 					<div
 						style={{ width: 600, margin: '0 auto' }}
-						ref={ref => (this.animBox1 = ref)}></div>
+						ref={ref => (this.hero05obj = ref)}></div>
+				</div>
+
+				<div className='anim'>
+					<h2>Mixpanel / Hero 07 / Group Analytics</h2>
+					<div
+						style={{ width: 600, margin: '0 auto' }}
+						ref={ref => (this.hero07obj = ref)}></div>
 				</div>
 			</div>
 		)
